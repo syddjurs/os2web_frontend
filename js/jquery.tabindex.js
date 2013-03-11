7b853ae31fa492c6730d369e63b8a6835137466a
Anders Bryrup <andersbryrup@gmail.com>
2013-03-11 15:08:32 +0100

    Initial Commit

---
+++ js/jquery.tabindex.js
@@ -0,0 +1,12 @@
+(function($)  {
+  $(function(){
+    var tabindex = 1;
+    $('a,select').each(function() {
+      if (this.type != "hidden") {
+        var $input = $(this);
+        $input.attr("tabindex", tabindex);
+        tabindex++;
+      }
+    });
+  });
+})(jQuery);
